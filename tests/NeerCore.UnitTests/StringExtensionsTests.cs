6d214489d97f73abdb8cf92d03bc0baafe836dd1
jurilents <keeeneeek@gmail.com>
2022-06-22 23:38:27 +0300

    Add NeerCore.Tests project

---
+++ tests/NeerCore.UnitTests/StringExtensionsTests.cs
@@ -0,0 +1,10 @@
+namespace NeerCore.UnitTests;
+
+public class StringExtensionsTests
+{
+	[Fact]
+	public void CamelCaseToWords_When_ValueIsValid() { }
+
+	[Fact]
+	public void CamelCaseToWords_When_ValueIsNull() { }
+}
