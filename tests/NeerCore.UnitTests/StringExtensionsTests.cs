bd244d61e0b9e04c5066d085884b73f10a5cc6a9
jurilents <keeeneeek@gmail.com>
2022-06-22 23:47:17 +0300

    Add test tests XD

--- tests/NeerCore.UnitTests/StringExtensionsTests.cs
+++ tests/NeerCore.UnitTests/StringExtensionsTests.cs
@@ -1,10 +1,27 @@
+using NeerCore.Extensions;
+
 namespace NeerCore.UnitTests;
 
 public class StringExtensionsTests
 {
 	[Fact]
-	public void CamelCaseToWords_When_ValueIsValid() { }
+	public void CamelCaseToWords_When_ValueIsValid()
+	{
+		string value = "CamelCase";
+		string expected = "Camel Case";
+
+		string actual = value.CamelCaseToWords();
+
+		Assert.Equal(expected, actual);
+	}
 
 	[Fact]
-	public void CamelCaseToWords_When_ValueIsNull() { }
+	public void CamelCaseToWords_When_ValueIsNullOrEmpty()
+	{
+		string emptyValue = string.Empty;
+		string nullValue = string.Empty;
+
+		Assert.Throws<ArgumentNullException>(() => emptyValue.CamelCaseToWords());
+		Assert.Throws<ArgumentNullException>(() => nullValue.CamelCaseToWords());
+	}
 }
