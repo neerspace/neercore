fe752d0d6fc8eece8d46e621f42a1e89ae1b1412
jurilents <keeeneeek@gmail.com>
2022-06-25 19:17:44 +0300

    Structuring the NeerCore.Data.Abstractions project

--- src/NeerCore.Data.Abstractions/IEntity.cs
+++ src/NeerCore.Data.Abstractions/IEntity.cs
@@ -1,7 +1,10 @@
 ﻿namespace NeerCore.Data.Abstractions;
 
+/// <summary>Defines entity.</summary>
 public interface IEntity { }
 
+/// <summary>Defines entity with the required <see cref="Id"/> property.</summary>
+/// <typeparam name="TKey"><see cref="Id"/> type.</typeparam>
 public interface IEntity<out TKey> : IEntity
 {
 	TKey Id { get; }
