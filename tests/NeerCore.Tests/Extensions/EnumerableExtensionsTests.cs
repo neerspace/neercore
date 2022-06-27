8b2a715c858839878de2948430a458012b268d94
jurilents <keeeneeek@gmail.com>
2022-06-27 10:06:52 +0300

    Add tests

---
+++ tests/NeerCore.Tests/Extensions/EnumerableExtensionsTests.cs
@@ -0,0 +1,29 @@
+﻿namespace NeerCore.Tests.Extensions;
+
+public class EnumerableExtensionsTests
+{
+	[Theory]
+	[InlineData(new object[] { "item_1", "item_2" }, true)]
+	[InlineData(new object[] { "item_1" }, true)]
+	[InlineData(new object[0], false)]
+	public void Test_FirstOr404(object[] source, bool expected)
+	{
+		if (expected)
+		{
+			object actual = source.FirstOr404();
+			Assert.Equal(source[0], actual);
+		}
+		else
+		{
+			Assert.Throws<NotFoundException>(source.FirstOr404);
+		}
+	}
+
+	[Theory]
+	[InlineData("item_1", "item_2")]
+	public void Test_FirstOr404_With_Predicate(params object[] source)
+	{
+		object actual = source.FirstOr404(x => (string) x == "item_2");
+		Assert.Equal("item_2", actual);
+	}
+}
---
+++ tests/NeerCore.Tests/Usings.cs
@@ -0,0 +1,3 @@
+global using Xunit;
+global using NeerCore.Exceptions;
+global using NeerCore.Extensions;
