0afe530749b364c7b95a2ffb105ee78a477ac550
jurilents <keeeneeek@gmail.com>
2022-07-01 01:56:24 +0300

    Update mapper extensions

--- src/NeerCore/Extensions/EnumerableExtensions.cs
+++ src/NeerCore/Extensions/EnumerableExtensions.cs
@@ -5,10 +5,12 @@ namespace NeerCore.Extensions;
 public static class EnumerableExtensions
 {
 	/// <summary>
-	/// Returns the first element of a sequence, or throws a <see cref="NotFoundException"/> if the sequence contains no elements.
+	///		Returns the first element of a sequence, or throws a
+	///		<see cref="NotFoundException"/> if the sequence contains no elements.
 	/// </summary>
 	/// <param name="source">The <see cref="IEnumerable{T}"/> to return the first element of.</param>
 	/// <typeparam name="TSource">The type of the elements of <paramref name="source"/>.</typeparam>
+	/// <returns>Found entity or throws 404 exception.</returns>
 	/// <exception cref="ArgumentNullException"><paramref name="source"/> is <see langword="null"/>.</exception>
 	/// <exception cref="NotFoundException"><paramref name="source"/> is empty.</exception>
 	public static TSource FirstOr404<TSource>(this IEnumerable<TSource> source)
