7fb3ec230d86276319fceacfc93776a363c71542
jurilents <keeeneeek@gmail.com>
2022-07-06 22:53:45 +0300

    Final refactor

---
+++ src/NeerCore/Exceptions/ConflictException.cs
@@ -0,0 +1,16 @@
+﻿using System.Net;
+
+namespace NeerCore.Exceptions;
+
+/// <summary>
+///   The exception represents an HTTP error with status <b>409</b>.
+///   Conflict <i>(indicates that the request could not be carried out because of a conflict on the server).</i>
+/// </summary>
+public class ConflictException : HttpException
+{
+	public override HttpStatusCode StatusCode => HttpStatusCode.Conflict;
+	public override string ErrorType => "Conflict";
+
+
+	public ConflictException(string message) : base(message) { }
+}
---
+++ src/NeerCore/Exceptions/UnprocessableEntityException.cs
@@ -0,0 +1,16 @@
+﻿using System.Net;
+
+namespace NeerCore.Exceptions;
+
+/// <summary>
+///   The exception represents an HTTP error with status <b>422</b>.
+///   Unprocessable entity <i>(indicates that the request was well-formed but was unable to be followed due to semantic errors).</i>
+/// </summary>
+public class UnprocessableEntityException : HttpException
+{
+	public override HttpStatusCode StatusCode => HttpStatusCode.UnprocessableEntity;
+	public override string ErrorType => "RequestEntityError";
+
+
+	public UnprocessableEntityException(string message) : base(message) { }
+}
