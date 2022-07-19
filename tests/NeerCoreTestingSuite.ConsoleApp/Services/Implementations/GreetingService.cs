d2b2f4c39e423c7247fa2d2c8a5ebd76369bfdba
jurilents <keeeneeek@gmail.com>
2022-07-19 23:44:28 +0300

    Add console DI test

---
+++ tests/NeerCoreTestingSuite.ConsoleApp/Services/Implementations/GreetingService.cs
@@ -0,0 +1,21 @@
+﻿using NeerCore.DependencyInjection;
+using NeerCoreTestingSuite.ConsoleApp.Services.Abstractions;
+
+namespace NeerCoreTestingSuite.ConsoleApp.Services.Implementations;
+
+[Injectable]
+internal class GreetingService : IGreetingService
+{
+	private readonly IMessageSender _sender;
+	public GreetingService(IMessageSender sender) => _sender = sender;
+
+	public void Greet(string target)
+	{
+		Say(target, "Hello World!");
+	}
+
+	public void Say(string target, string message)
+	{
+		_sender.Send($"Hey, {target}! {message}");
+	}
+}
