bc1659055c311faf1a8540d1089f39871cb9b7ff
jurilents <keeeneeek@gmail.com>
2022-08-01 22:39:07 +0300

    Add paging extensions

--- src/NeerCore.Api/Extensions/HttpResponseExtensions.cs
+++ src/NeerCore.Api/Extensions/HttpResponseExtensions.cs
@@ -7,7 +7,30 @@ public static class HttpResponseExtensions
     /// <summary>
     ///   Adds pagination info to the current response headers.
     ///   And also adds CORS header to allow the browser use pagination headers.
+    ///   <br/>
+    ///   Sets next headers:
+    ///   <list type="bullet">
+    ///     <item><b>Navigation-Page</b>: Current navigation page.</item>
+    ///     <item><b>Navigation-Last-Page</b>: Max navigation page value.</item>
+    ///     <item><b>Navigation-Total</b>: Totals count of filtered items (without pagination).</item>
+    ///     <item><b>Access-Control-Expose-Headers</b>: Allows previous headers in CORS.</item>
+    ///   </list>
     /// </summary>
+    /// <remarks>For paging navigation style.</remarks>
+    /// <example>
+    ///   Controller action pseudo code:
+    ///   <br/>
+    ///   <code>
+    ///     [HttpGet]
+    ///     public IEnumerable&lt;Model&gt; GetList(string filters, int page = 1, int pageSize = 10)
+    ///     {
+    ///       IEnumerable&lt;Model&gt; models = _service.Filter(filters, page, pageSize);
+    ///       int total = _service.Count(filters);
+    ///       Response.SetNavigationHeaders(total, page, pageSize);
+    ///       return models;
+    ///     }
+    ///   </code>
+    /// </example>
     /// <param name="response">Represents the outgoing side of an individual HTTP request.</param>
     /// <param name="total">Total count of all entities.</param>
     /// <param name="page">Current navigation page. (starts from 1)</param>
@@ -16,7 +39,46 @@ public static class HttpResponseExtensions
     {
         response.Headers["Navigation-Page"] = page.ToString();
         response.Headers["Navigation-Last-Page"] = ((int)Math.Ceiling((double)total / pageSize)).ToString();
-        response.Headers["Count"] = total.ToString();
-        response.Headers["Access-Control-Expose-Headers"] = "Navigation-Page,Navigation-Last-Page,Count";
+        response.Headers["Navigation-Total"] = total.ToString();
+        response.Headers["Access-Control-Expose-Headers"] = "Navigation-Page,Navigation-Last-Page,Navigation-Total";
+    }
+
+    /// <summary>
+    ///   Adds pagination info to the current response headers.
+    ///   And also adds CORS header to allow the browser use pagination headers.
+    ///   <br/>
+    ///   Sets next headers:
+    ///   <list type="bullet">
+    ///     <item><b>Navigation-Page</b>: Current navigation page.</item>
+    ///     <item><b>Navigation-Last-Page</b>: Max navigation page value.</item>
+    ///     <item><b>Navigation-Total</b>: Totals count of filtered items (without pagination).</item>
+    ///     <item><b>Access-Control-Expose-Headers</b>: Allows previous headers in CORS.</item>
+    ///   </list>
+    /// </summary>
+    /// <remarks>For skip-take navigation style.</remarks>
+    /// <example>
+    ///   Controller action pseudo code:
+    ///   <br/>
+    ///   <code>
+    ///     [HttpGet]
+    ///     public IEnumerable&lt;Model&gt; GetList(string filters, int skip = 0, int take = 10)
+    ///     {
+    ///       IEnumerable&lt;Model&gt; models = _service.Filter(filters, skip, take);
+    ///       int total = _service.Count(filters);
+    ///       Response.SetPaginationHeaders(total, skip, take);
+    ///       return models;
+    ///     }
+    ///   </code>
+    /// </example>
+    /// <param name="response">Represents the outgoing side of an individual HTTP request.</param>
+    /// <param name="total">Total count of all entities.</param>
+    /// <param name="skip">Index of first element in the result sequence.</param>
+    /// <param name="take">Count of items in result sequence.</param>
+    public static void SetPaginationHeaders(this HttpResponse response, int total, int skip, int take)
+    {
+        response.Headers["Navigation-Page"] = ((int)Math.Ceiling((double)skip / take)).ToString();
+        response.Headers["Navigation-Last-Page"] = ((int)Math.Ceiling((double)total / take)).ToString();
+        response.Headers["Navigation-Total"] = total.ToString();
+        response.Headers["Access-Control-Expose-Headers"] = "Navigation-Page,Navigation-Last-Page,Navigation-Total";
     }
 }
